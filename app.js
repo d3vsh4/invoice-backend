08218fccf0724eea0c42feb99859b61c20284a53
d3vsh4 <sdevanshu12345@gmail.com>
2021-05-17 21:29:10 +0530

    revert

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var quotesRouter = require('./routes/quotes');
-const apiRoute = require('./routes/api');
+// var indexRouter = require('./routes/index');
+// var quotesRouter = require('./routes/quotes');
+// const apiRoute = require('./routes/api');
 
 var app = express();
 
@@ -17,9 +17,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
 // app.use('/quotes', quotesRouter);
+app.use(cors());
+
 app.get('/', (req,res)=>{
     res.send("yes it worked")
 })
 
 app.use('/api',apiRoute);
+
+
 module.exports = app;
